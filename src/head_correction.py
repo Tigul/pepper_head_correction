d63889c5fb67646c7bc5a4e346ddbec3f7fba417
Jonas Dech <j.dech@gmx.net>
2021-11-28 15:54:03 +0100

    doc

--- src/head_correction.py
+++ src/head_correction.py
@@ -14,7 +14,10 @@ from pepper_behaviour_srvs.srv import MoveArm, MoveHead, Speak
 
 def move_head(session, msg):
     """
-    This example uses the setAngles method.
+    Moves the head joints, HeadYaw and HeadPitch into the positions specified in
+    the Message.
+    :param Session: The NAOqi session that connects this node to Pepper.
+    :param msg: The ROS message that contains the goal joint values.
     """
     # Get the service ALMotion.
 
@@ -34,6 +37,13 @@ def move_head(session, msg):
     return "ok"
 
 def move_arm(session, msg):
+    """
+    Moves one arm into the position specified in the Message. The message consists
+    of a list with the joint position for every joint in the arm. The message also
+    contains the arm which should be moved.
+    :param session: The NAOqi session which connects this node to the Pepper.
+    :param msg: The ROS message which contains the arm as well as the goal joint states.
+    """
     motion_service  = session.service("ALMotion")
     arm = "L" if msg.arm == "left" else "R"
     joint_poses = msg.positions
@@ -41,17 +51,26 @@ def move_arm(session, msg):
     motion_service.setStiffnesses(arm + "Arm", 1.0)
 
     neutral_names = ['ShoulderPitch', 'ShoulderRoll', 'ElbowYaw', 'ElbowRoll', 'WristYaw']
+    # Create correct joint names for the chosen arm
     names = [arm + n for n in neutral_names]
 
     fractionMaxSpeed = 0.2
 
     motion_service.setAngles(names, joint_poses, fractionMaxSpeed)
+    # Needs to be here since the previous statement is non-blocking and otherwise
+    # the stiffness would be set while the arm is still moving.
     time.sleep(3.0)
     motion_service.setStiffnesses(arm + "Arm", 0.5)
     rospy.loginfo(f"Moved {msg.arm} Arm")
     return "ok"
 
 def speak(session, msg):
+    """
+    Sends a string to the Text-to-Speech engine of NAOqi which generates spoken text
+    and plays it over the speaker located in Peppers head.
+    :param session: The NAOqi session which connects to Pepper.
+    :param msg: The ROS message containing the text to be spoken.
+    """
     tts = session.service("ALTextToSpeech")
 
     tts.say(msg.text)
